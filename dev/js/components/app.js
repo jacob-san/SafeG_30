f59e14d460dbc557c431a0437b3c1e4ef144ca5e
Sandeep <sandeepmukkadan@gmail.com>
2017-06-21 09:42:52 +0530

    first

--- dev/js/components/app.js
+++ dev/js/components/app.js
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import UserList from '../containers/user-list';
+import UserDetail from  '../containers/user-detail'
 require('../../scss/style.scss')
 
 const App = ()=>(
@@ -11,6 +12,7 @@ const App = ()=>(
       <UserList/>
       <hr/>
       <h2>User Details:</h2>
+      <UserDetail />
   </div>
 );
 export default App;
--- dev/js/reducers/index.js
+++ dev/js/reducers/index.js
@@ -3,8 +3,10 @@
  */
 import {combineReducers} from 'redux';
 import UserReducer from './reducer-users';
+import ActiveUserReducer from './reducer-active-user'
 
 const allReducers = combineReducers({
-    users:UserReducer
+    users:UserReducer,
+    activeUser: ActiveUserReducer
 })
 export default allReducers;
